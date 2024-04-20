4a3f4396a2cfad39a1f500b8fc15d73cf8ceebe9
andreresende36 <andreresende36@gmail.com>
2024-04-20 19:08:19 -0300

    chore: excluding some files

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -5,10 +5,6 @@ import { Request, Response } from 'express';
 import productsRoutes from './routes/products.routes';
 import packsRoutes from './routes/packs.routes';
 
-// import multer from 'multer';
-// import validateFile from './utils/validation'; // Implemente esta função para validar o arquivo CSV
-// import processFile from './utils/processing'; // Implemente esta função para processar o arquivo CSV
-
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -21,7 +17,5 @@ app.get('/', (_request: Request, response: Response) => {
 
 app.use('/products', productsRoutes);
 app.use('/packs', packsRoutes);
-// Configuração do multer para upload de arquivos
-// const upload = multer({ dest: 'uploads/' });
 
 export default app;
--- app/frontend/src/context/AppContext.ts
+++ app/frontend/src/context/AppContext.ts
@@ -1,5 +1,4 @@
 import { createContext } from "react";
-// import { AppContextType } from '../interfaces/AppContextType';
 
 const AppContext = createContext(null as any);
 
