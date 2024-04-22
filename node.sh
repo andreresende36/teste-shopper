e9a4a0d09b8be34659be32b9e8e75153d7371986
andreresende36 <andreresende36@gmail.com>
2024-04-21 23:51:04 -0300

    edit README.md

--- node.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install curl
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | sh
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install node 20.12.2
